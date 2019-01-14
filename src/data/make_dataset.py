fffd40f57d3f0a41f3cb7704041ec22327e19858
Zamora <Lzamora@agea.com.ar>
2019-01-14 18:03:36 -0300

    Se deja funcionando el modulo make file.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,46 +1,57 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 from features import build_features
+from helpers import hand_data, constans
 
 
-def __drop_columns(dataframe, columns=[]):
-    return dataframe.drop(columns=columns)
+def __set_index(df, col_index):
+    return df.set_index(col_index)
 
-def __drop_duplicates(dataframe, columns=None):
-    return dataframe.drop_duplicates(subset=columns)
 
-def __convert_type(dataframe, **kwargs):
-    for column, type in kwargs.items():
-        if type == 'int':
-            dataframe[column] = dataframe[column].astype(int)
-        elif type == 'date':
-            dataframe[column] = pd.to_datetime(dataframe[column])
-    return dataframe
+def __drop_columns(df, columns=[]):
+    return df.drop(columns=columns)
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+
+def __drop_duplicates(df, columns=None):
+    return df.drop_duplicates(subset=columns)
+
+
+def __convert_type(df, dict_var):
+    for column, data_type in dict_var.items():
+        if data_type == 'int':
+            df[column] = df[column].astype(int)
+        elif data_type == 'date':
+            df[column] = pd.to_datetime(df[column])
+    return df
+
+
+def main(input_file_path, output_file_path):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+    args_convert = {'price': 'int', 'bathrooms': 'int', 'floors': 'int', 'date': 'date'}
+
+    # loading data
+    df = hand_data.load_data(input_file_path, 'csv')
+    # cleaning data
+    df = __set_index(df, 'id')
+    df = __drop_duplicates(df)
+    df = __convert_type(df, args_convert)
+    # feature engineering
+    df = build_features.add_column_house_age(df, 'date', 'yr_built')
+    df = build_features.add_column_renovated(df, 'yr_renovated')
+    df = __drop_columns(df, ['date', 'yr_renovated'])
+    # saving data
+    hand_data.save_data(df, output_file_path, 'test.csv')
+
+    logger.info('Making final data set from raw data.')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(constans.DATA_RAW_PATH, constans.OUTPUT_DATA_PROCESS_PATH)
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-def add_column_house_age(dataframe, column_date, columns_date_build):
-    dataframe["house_age"] = dataframe["date"].dt.year - dataframe['yr_built']
-    return dataframe
 
-def add_column_renovated(dataframe, column_date_renovated):
-    dataframe["renovated"] = dataframe[column_date_renovated].apply(lambda yr: 0 if yr == 0 else 1)
-    return dataframe
+def add_column_house_age(df, column_date, columns_date_build):
+    df["house_age"] = df[column_date].dt.year - df[columns_date_build]
+    return df
+
+
+def add_column_renovated(df, column_date_renovated):
+    df["renovated"] = df[column_date_renovated].apply(lambda yr: 0 if yr == 0 else 1)
+    return df
